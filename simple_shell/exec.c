a9c0c97aca8309204f2c94c340b8fa8bc89dfe41
EdlawitG <88848586+EdlawitG@users.noreply.github.com>
2022-05-18 09:23:29 -0900

    simple_shell

---
+++ simple_shell/exec.c
@@ -0,0 +1,36 @@
+#include "simple_shell.h"
+/**
+*Ghassen - Ala 
+*_exec - execute the command
+*@cmd: the command
+*@array: array holdin the argments
+*Return: Return 0 in success
+*/
+int _exec(char *cmd, char **array)
+{
+	pid_t pid;
+	int status;
+
+	if (cmd == NULL || array == NULL)
+		return (-1);
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fail to fork");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(cmd, array, environ) != 0)
+		{
+			perror("fail to execute");
+			return (-1);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+	}
+	return (1);
+}
